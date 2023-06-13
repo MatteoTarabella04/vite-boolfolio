e7c39df085ad716d0fcc2f825dbcd150386288bc
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-13 15:56:16 +0200

    install axios + impost and call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script>
 
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      API_URL: 'http://127.0.0.1:8000/',
+      PROJECTS_PATH: 'api/projects',
+      projects: [],
+    }
+  }, method: {
+    getProjects(url) {
+      axios.get(url)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        })
+
+    }
+  }, mounted() {
+    const url = this.API_URL + this.PROJECTS_PATH
+    console.log(url);
+  }
+}
+
 </script>
 
 <template>
