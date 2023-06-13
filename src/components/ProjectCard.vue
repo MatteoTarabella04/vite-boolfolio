c7cf55e4456053ec0667598ff9af53de24c0e9d4
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-13 16:58:44 +0200

    create card component for projects

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+   name: "ProjectCard",
+   props: {
+      name: String,
+      link: String,
+      description: String,
+   }
+}
+
+</script>
+
+<template>
+   <div class="col">
+      <div class="card p-0">
+         <div class="card-header">
+            <h3 class="card-title">{{ name }}</h3>
+         </div>
+         <div class="card-body">
+            <p>
+               {{ description }}
+            </p>
+            <a target="_blank" :href="link">{{ link }}</a>
+         </div>
+      </div>
+   </div>
+</template>
