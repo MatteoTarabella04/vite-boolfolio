35d2b60de8656693fb0705a3e2cb0c9f3d4d79cc
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-14 16:08:50 +0200

    create app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+
+   name: 'AppHeader'
+
+}
+</script>
+<template>
+   <header class="shadow d-flex align-items-center">
+      <div class="logo px-2">
+         logo
+      </div>
+      <nav class="nav justify-content-center">
+         <a class="nav-link" href="#" aria-current="page">Home</a>
+         <a class="nav-link" href="#">About</a>
+         <a class="nav-link" href="#">Contacts</a>
+      </nav>
+   </header>
+</template>
+
+
+<style lang="scss" scoped></style>
