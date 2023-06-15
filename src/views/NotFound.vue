9ceaf41ad5e8b02c28ca52d1d075dbecbbaa821a
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-15 16:28:38 +0200

    create 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+   name: "NotFound",
+}
+</script>
+
+<template>
+   <div class="position-fixed start-50 top-50 translate-middle py-5 text-center">
+      <h1>404 | NOT FOUND</h1>
+      <router-link class="text_custom_green" :to="{ name: 'home' }">HOME</router-link>
+   </div>
+</template>
