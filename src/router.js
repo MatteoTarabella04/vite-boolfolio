1e84db1ee879d6e6bef0a093d727b4b920641593
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-15 16:27:12 +0200

    add 4004 page route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./views/AppHome.vue";
 import SingleProject from "./views/SingleProject.vue";
+import NotFound from "./views/NotFound.vue";
 
 const router = createRouter({
    history: createWebHistory(),
@@ -15,6 +16,11 @@ const router = createRouter({
          name: "single-project",
          component: SingleProject,
       },
+      {
+         path: "/:pathMatch(.*)*",
+         name: "not-found",
+         component: NotFound,
+      }
    ]
 });
 
