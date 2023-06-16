e6e533068d63a972a05b436fb54738993ecf7bb0
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-16 09:40:57 +0200

    fix  404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -7,6 +7,6 @@ export default {
 <template>
    <div class="position-fixed start-50 top-50 translate-middle py-5 text-center">
       <h1>404 | NOT FOUND</h1>
-      <router-link class="text_custom_green" :to="{ name: 'home' }">HOME</router-link>
+      <router-link :to="{ name: 'home' }">HOME</router-link>
    </div>
 </template>
