4e67b26400edfe28e64be3f141c2fff48a116193
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-19 09:37:10 +0200

    call api

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -1,6 +1,6 @@
 <script>
 
-import axios from 'axios';
+import axios from "axios"
 
 export default {
 
@@ -10,17 +10,23 @@ export default {
    data() {
       return {
          API_URL: 'http://127.0.0.1:8000/',
-         PROJECTS_PATH: 'api/projects',
-         project: null,
+         project: null
       }
    },
 
    mounted() {
-      axios.get()
+
+      const url = this.API_URL + 'api/projects/' + this.$route.params.slug;
+      console.log(url);
+
+      axios
+         .get(url)
          .then((response) => {
+
+            console.log(response);
+
             if (response.data.success) {
-               this.project = response.data.project;
-               console.log(this.project);
+               this.project = response.data.result;
             } else {
                // redirect alla pagina 404
                this.$router.push({ name: 'not-found' })
@@ -33,38 +39,7 @@ export default {
 
 }
 </script>
-<template>
-   <div class="container">
-      <div class="card m-auto h-100 p-0">
-         <div class="card-header">
-            <h3 class="card-title">{{ project.name }}</h3>
-         </div>
-         <div class="card-body">
-            <p>
-               {{ project.description }}
-            </p>
-            <p>
-               <b v-if="project.technologies">Technologies:</b>
-            </p>
-            <ul class="d-flex flex-wrap gap-2 list-unstyled" v-if="project.technologies">
-               <li v-for="tech in project.technologies">
-
-                  <span class="badge bg-primary">
-                     {{ tech.name }}
-                  </span>
-
-               </li>
-            </ul>
-            <div v-if="project.type" class="type">
-               <p><b>Type: </b>{{ project.type.name }}</p>
-            </div>
-            <a target="_blank" :href="link">{{ link }}</a>
-         </div>
-         <div class="card-footer">
-         </div>
-      </div>
-   </div>
-</template>
+<template></template>
 
 
 <style lang="scss" scoped></style>
