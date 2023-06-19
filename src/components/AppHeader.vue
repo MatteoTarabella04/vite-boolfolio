3c90b3397deaf6b819bd5802c107595752f72a40
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-19 09:35:46 +0200

    fix link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,7 +11,7 @@ export default {
          logo
       </div>
       <nav class="nav justify-content-center">
-         <RouterLink class="nav-link" :to="{ name: 'home' }" aria-current="page">Home</RouterLink>
+         <RouterLink class="nav-link" :to="{ name: 'home' }">Home</RouterLink>
          <a class="nav-link" href="#">About</a>
          <a class="nav-link" href="#">Contacts</a>
       </nav>
